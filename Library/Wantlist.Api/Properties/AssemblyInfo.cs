2899ad49e5bb708b78dbaf4d0184eec32b249636
andrew shearer <shearer3000@gmail.com>
2013-11-09 14:02:06 +1300

    library api

---
+++ Library/Wantlist.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wantlist.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dbdf051-c67f-4f0e-b0ea-026f786505b2")]
+
